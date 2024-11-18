0f688f0ce4fcdce1e123e7c8cd1abe9217e4cafe
Lívia Santos <livia.rosaraujo3@gmail.com>
2024-11-18 15:51:32 -0300

    feat: remove the 'only'

--- cypress/e2e/Login/register_user.cy.js
+++ cypress/e2e/Login/register_user.cy.js
@@ -5,7 +5,7 @@ describe('Test Case 1: Register User', () => {
         cy.visit('https://automationexercise.com/login')
     })
 
-    it.only('Register new user and delete', () => {
+    it('Register new user and delete', () => {
         cy.get('header').should('be.visible')
         cy.contains('Signup / Login').click()
         cy.contains('New User Signup!').should('be.visible')
@@ -55,20 +55,21 @@ describe('Test Case 1: Register User', () => {
        
 
     it('Register new user', () => {
-        cy.contains('Signup / Login').click()
-
-        cy.get('[data-qa="signup-name"]').type('registerUser.name')
-        cy.get('[data-qa="signup-email"]').type('livia.qa@teste.com.br')
-        cy.get('[data-qa="signup-button"]').click()
-    
-        cy.contains('Enter Account Information').should('be.visible')
 
         cy.fixture('registerUser').then((registerUser) => {
+
+            cy.contains('Signup / Login').click()
+
+            cy.get('[data-qa="signup-name"]').type(registerUser.name)
+            cy.get('[data-qa="signup-email"]').type('qateste@teste.com.br')
+            cy.get('[data-qa="signup-button"]').click()
+    
+            cy.contains('Enter Account Information').should('be.visible')
 
             cy.get('#id_gender1').check()
             cy.get('#name').clear()
             cy.get('#name').type('Lívia QA')
-            cy.get('#password').type('registerUser.password')
+            cy.get('#password').type(registerUser.password)
             cy.get('select#days').select('25')
             cy.get('select#months').select('March')
             cy.get('select#years').select('2004')
@@ -78,13 +79,13 @@ describe('Test Case 1: Register User', () => {
             cy.get('#first_name').type('Lívia')
             cy.get('#last_name').type('QA')
             cy.get('#company').type('Treinando')
-            cy.get('#address1').type('registerUser.address_1')
-            cy.get('#address2').type('registerUser.address_2')
+            cy.get('#address1').type(registerUser.address_1)
+            cy.get('#address2').type(registerUser.address_2)
             cy.get('select#country').select('New Zealand')
-            cy.get('#state').type('registerUser.state')
-            cy.get('#city').type('registerUser.city')
-            cy.get('#zipcode').type('registerUser.zipcode')
-            cy.get('#mobile_number').type('registerUser.number')
+            cy.get('#state').type(registerUser.state)
+            cy.get('#city').type(registerUser.city)
+            cy.get('#zipcode').type(registerUser.zipcode)
+            cy.get('#mobile_number').type(registerUser.number)
             
             cy.get('[data-qa="create-account"]').click()
 
